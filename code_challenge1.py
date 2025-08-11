63155917bc7b94d4517eda9b25508eeb482a1433
Pastorfide, Oswald <pastorfideoswald@gmail.com>
2025-08-11 18:11:25 +0800

    Delete code_challenge1.py

--- code_challenge1.py
+++
@@ -1,8 +0,0 @@
-name = input("Type your name-->")
-
-#print("\t\t\t\t\t*\n\t\t\t\t*\t\t*\n\t\t\t*\t\t\t\t*\n\t\t*\t\tHi",name,"\t\t\t*\n\t*\t\t\t\t\t\t\t\t*\n\t\t*\t\t\t\t\t\t*\n\t\t\t*\t\t\t\t*\n\t\t\t\t*\t\t*\n\t\t\t\t\t*")
-
-
-#print("\t\t\t\t\t*\n\t\t\t\t*\t\t*\n\t\t\t*\t\t\t\t*\n\t\t*\t\t\t\t\t\t*\n\t*\t\t\tHi",name,"\t\t\t\t*\n\t\t*\t\t\t\t\t\t*\n\t\t\t*\t\t\t\t*\n\t\t\t\t*\t\t*\n\t\t\t\t\t*")
-
-print("\t\t\t\t\t*\n\t\t\t\t*\t\t*\n\t\t\t*\t\t\t\t*\n\t\t*\t\t\t\t\t\t*\n\t*\t\t\tHi\n\t\t\t\t\t",name,"\t\t\t*\n\t\t*\t\t\t\t\t\t*\n\t\t\t*\t\t\t\t*\n\t\t\t\t*\t\t*\n\t\t\t\t\t*")
