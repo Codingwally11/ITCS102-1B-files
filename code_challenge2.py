08c3e780944815cec0244ea887379b6989714b16
Pastorfide, Oswald <pastorfideoswald@gmail.com>
2025-08-15 08:18:31 +0800

    code_challenge2.py

---
+++ code_challenge2.py
@@ -0,0 +1,31 @@
+price = eval(input("Enter amount to deposit "))
+print("Here's the counting amount of the ")
+
+a1 = 1000 
+a2 = 500
+a3 = 200
+a4 = 100 
+a5 = 50
+a6 = 20
+a7 = 10
+a8 = 5 
+a9 = 1
+
+print(price // a1, "=", a1)
+price = price % a1
+print(price // a2, "=", a2)
+price = price % a2
+print(price // a3, "=", a3)
+price = price % a3
+print(price // a4, "=", a4)
+price = price % a4
+print(price // a5, "=", a5)
+price = price % a5
+print(price // a6, "=", a6)
+price = price % a6
+print(price // a7, "=", a7)
+price = price % a7
+print(price // a8, "=", a8)
+price = price % a8
+print(price // a9, "=", a9)
+price = price % a9
