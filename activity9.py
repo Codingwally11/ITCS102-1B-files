1ae11188d46cf267e863a26b7c65098ccd531979
Pastorfide, Oswald <pastorfideoswald@gmail.com>
2025-08-18 15:47:41 +0800

    activity9.py

---
+++ activity9.py
@@ -0,0 +1,13 @@
+a = 9
+b = 9
+
+c = 11 
+d = 10
+
+username = 'oswald'
+password = 'dorabox09'
+print((username == 'oswald') and (password == 'dorabox09'))
+
+#print ( (a > b) and (c < d) )
+#print ( (a > b) or (c < d) )
+#print(not((a > b) or (c < d)))
