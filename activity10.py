eff7168649e34c9c51a0306b94ea560bb78f6b40
Pastorfide, Oswald <pastorfideoswald@gmail.com>
2025-08-27 15:07:24 +0800

    activity10.py

---
+++ activity10.py
@@ -0,0 +1,10 @@
+username = "wallypogi"
+password = "pogitalaga123"
+
+uname = input("Enter username --> ")
+pword = input("Enter password --> ")
+
+if (uname == username) and (pword == password):
+	print("ACCES GRANTED")
+else:
+	print("ACCESS DENIED")
